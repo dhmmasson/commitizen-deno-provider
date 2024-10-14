dc92933a8af57bb4e5bded2d0c4cc543833398f1
dhmmasson <5263585+dhmmasson@users.noreply.github.com>
2024-10-14 19:09:43 +0200

    fix: move deno_provide to its own folder

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='commitizen-deno-provider',
-    version='0.1.0',
-    py_modules=['deno_provider'],
+    version='0.1.2',
+    py_modules=['deno_provider/deno_provider'],
     install_requires=['commitizen'],
     entry_points={
         'commitizen.provider': [
-            'deno-provider = deno_provider:DenoProvider',
+            'deno-provider = deno_provider.deno_provider:DenoProvider',
         ]
     }
 )
